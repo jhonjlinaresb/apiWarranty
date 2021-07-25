bd4873b3c26b58ae2b443d93d0a2905c78fe38c9
unknown <jhonjlinaresb@gmail.com>
2021-07-25 18:40:23 +0200

    Limpieza de código comentado

--- public/app.js
+++ public/app.js
@@ -1,60 +1,25 @@
-//Conexión con axios
 //Obtener todos los clientes-------------------
-  /* const getClientes = () => {
-    axios.get('/garantia/clientes')
-    .then(response => {
-    const clientes = response.data;
-    console.log(`GET Clientes`, clientes);
-    //html.innerHTML=(`GET Clientes`, clientes);
-    })
-     .catch(error => console.error(error));
-    };
-    getClientes(); */
-//---------------------------------------------
-/* var obj = {
-    "nombre" : String,
-    "numero": Number,
-    "direccion": String,
-    "estado": Boolean,
-    "fechaInicio": Date,
-    "fechaFin": Date
-}
-var html = document.getElementById("html");
-html.innerHTML+="<p>nombre="+obj.getClientes.nombre;
-html.innerHTML+="<p>numero="+getClientes.numero; */
-/* import axios from 'axios';
-export default {
-  data() {
-    return {
-      clients: [],
-      errors: []
-    }
-  },
-  created() {
-    axios.get('/garantia/clientes')
-    .then(response => {
-      this.clients = response.data
-    })
-    .catch(e => {
-      this.errors.push(e)
-    })
-  }
-} */
-
-var vm = new Vue({
-  el: '#app',
-  data: {
-    posts: [
-      {id: "_id", estado: "estado", nombre: "nombre", numero: "numero", direccion: "direccion", fechaInicio: "fechaInicio", fechaFin: "fechaFin"},
-      //{id: _id, estado: estado, nombre: nombre, numero: numero, direccion: direccion, fechaInicio: fechaInicio, fechaFin: fechaFin},
-      //{id: _id, estado: estado, nombre: nombre, numero: numero, direccion: direccion, fechaInicio: fechaInicio, fechaFin: fechaFin}
+// Se  crea una variable para cargar los datos de los clientes
+var vm = new Vue({ //Se genera un nuevo Vue
+  el: '#app',//Llama a la etiqueta de la pagina
+  data: { //Array de datos para pintar en el HTML
+    posts: [ //Array de datos
+      {
+      id: "_id", 
+      estado: "estado", 
+      nombre: "nombre", 
+      numero: "numero", 
+      direccion: "direccion", 
+      fechaInicio: "fechaInicio", 
+      fechaFin: "fechaFin"
+      }
     ]
   },
   methods: {
-    getClientes: function() {
-      axios.get('/garantia/clientes')
-        .then(response => {this.posts = response.data})
-        console.log('Hola, he funcionado :D', this);
+    getClientes: function() { //Método para obtener los clientes
+      axios.get('/garantia/clientes') //Petición a la API con Axios
+        .then(response => {this.posts = response.data}) //Luego genera una respuesta y se asigna el array de datos a la variable posts
+        console.log('Hola, he funcionado :D', this); //Muestra en consola un mensaje de éxito
     }
   }
 });
